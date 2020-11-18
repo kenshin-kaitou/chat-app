3041183592acb600dc1c7e013faf4c1e3b021eda
kenshin-kaitou <60423083+kenshin-kaitou@users.noreply.github.com>
2020-11-18 17:07:36 +0900

    Revert "新規作成画面の表示"
    This reverts commit 35a89e8c7b216e57884aceffc2d129de067bc4ea.

--- app/controllers/rooms_controller.rb
+++
@@ -1,5 +0,0 @@
-class RoomsController < ApplicationController
-	def new
-		
-	end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_073336) do
-
-  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_11_18_050941) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
